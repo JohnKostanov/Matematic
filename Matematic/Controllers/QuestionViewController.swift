2e8c97f39a891c07cbd207fc352266782238b579
JohnKostanov <kostanovd@icloud.com>
2019-08-03 23:24:54 +0200

    Add actionsButtons in QuestionViewController

--- Matematic/Controllers/QuestionViewController.swift
+++ Matematic/Controllers/QuestionViewController.swift
@@ -13,6 +13,7 @@ class QuestionViewController: UIViewController {
     @IBOutlet var summaBasicStackView: UIStackView!
     
     @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var numbersInputLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
     @IBOutlet var numbersButtons: [UIButton]!
@@ -29,9 +30,7 @@ class QuestionViewController: UIViewController {
         updateUI()
     }
     
-    @IBAction func buttonAction(_ sender: UIButton) {
-       updateUI()
-    }
+    
     
     func nextQuestion() {
         questionIndex += 1
@@ -42,8 +41,29 @@ class QuestionViewController: UIViewController {
         let numberSecond = question[questionIndex].numberSecond
         let currentAnswer = numberFirst + numberSecond
         questionLabel.text = question[questionIndex].questionType + " " + String(numberFirst) + " " + "+" + " " + String(numberSecond)
-        answerLabel.text = "Правильный ответ: " + String(currentAnswer)
+        answerLabel.text = String(currentAnswer)
         nextQuestion()
     }
-
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        updateUI()
+        numbersInputLabel.text! = ""
+    }
+    
+    @IBAction func numbersAnswer(_ sender: UIButton) {
+        guard let numbersText = sender.titleLabel?.text else { return }
+//        guard var answerLabel = answerLabel.text else { return }
+//        numbersInputLabel.text! = ""
+        numbersInputLabel.text! += numbersText
+        print(numbersText)
+    }
+    
+    @IBAction func examinationButtonPressed(_ sender: UIButton) {
+        if numbersInputLabel.text == answerLabel.text {
+            numbersInputLabel.text = "Ответ верный"
+        } else {
+             numbersInputLabel.text = "Не правильно"
+        }
+    }
+    
 }
